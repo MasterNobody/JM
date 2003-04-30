4f2099b1f910c66b31cd778cf252dc931ffc7f0c
Dong Tian <tian@cs.tut.fi>
2003-04-30 16:26:00 +0800

    Import jm6.2

--- lencod/inc/output.h
+++ lencod/inc/output.h
@@ -31,40 +31,18 @@
 */
 
 /*!
- ***************************************************************************
+ **************************************************************************************
  * \file
- *    biaridecod.h
- *
+ *    output.h
  * \brief
- *    Headerfile for binary arithmetic decoder routines
- *
+ *    Picture writing routine headers
  * \author
- *    Detlev Marpe,
- *    Gabi Blättermann
- *    Copyright (C) 2000 HEINRICH HERTZ INSTITUTE All Rights Reserved.
- *
- * \date
- *    21. Oct 2000
- **************************************************************************
+ *    Main contributors (see contributors.h for copyright, address and affiliation details) 
+ *      - Karsten Suehring        <suehring@hhi.de>
+ ***************************************************************************************
  */
 
-#ifndef _BIARIDECOD_H_
-#define _BIARIDECOD_H_
-
-
-/************************************************************************
- * D e f i n i t i o n s
- ***********************************************************************
- */
-
-void arideco_start_decoding(DecodingEnvironmentPtr eep, unsigned char *code_buffer, int firstbyte, int *code_len, int slice_type);
-int  arideco_bits_read(DecodingEnvironmentPtr dep);
-void arideco_done_decoding(DecodingEnvironmentPtr dep);
-void biari_init_context (struct img_par *img, BiContextTypePtr ctx, const int* ini);
-void rescale_cum_freq(BiContextTypePtr bi_ct);
-unsigned int biari_decode_symbol(DecodingEnvironmentPtr dep, BiContextTypePtr bi_ct );
-unsigned int biari_decode_symbol_eq_prob(DecodingEnvironmentPtr dep);
-unsigned int biari_decode_final(DecodingEnvironmentPtr dep);
-
-#endif  // BIARIDECOD_H_
-
+void write_stored_frame(FrameStore *fs, FILE *p_out);
+void direct_output(StorablePicture *p, FILE *p_out);
+void init_out_buffer();
+void uninit_out_buffer();
